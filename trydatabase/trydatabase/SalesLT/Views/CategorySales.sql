0ea5a2278311d159ac6a6ccb1b67512e11ff2713
wiebedebois <114914216+wiebedebois@users.noreply.github.com>
2022-10-05 10:32:28 +0200

    second try
    second try

---
+++ trydatabase/trydatabase/SalesLT/Views/CategorySales.sql
@@ -0,0 +1,11 @@
+﻿CREATE VIEW Saleslt.CategorySales
+AS
+SELECT  c.Name AS Category,
+        o.CustomerID AS Cust,
+        od.OrderQty AS Qty,
+        YEAR(o.orderdate) AS Orderyear
+FROM    SalesLT.ProductCategory AS c
+        INNER JOIN SalesLT.product AS p ON c.ProductCategoryID=p.ProductCategoryID
+        INNER JOIN Saleslt.SalesOrderDetail AS od ON p.productid=od.productid
+        INNER JOIN Saleslt.SalesOrderHeader AS o ON od.SalesOrderID=o.SalesOrderID
+--WHERE c.ParentProductCategoryID IN (1,2,3,)
---
+++ trydatabase/trydatabase/dbo/Functions/fn_LineTotal.sql
@@ -0,0 +1,8 @@
+﻿CREATE FUNCTION dbo.fn_LineTotal ( @orderid INT )
+RETURNS TABLE
+AS
+RETURN
+    SELECT  SalesOrderID, productid, unitprice, OrderQty, UnitPriceDiscount,
+            CAST(( OrderQty * unitprice * ( 1 - UnitPriceDiscount ) ) AS DECIMAL(8, 2)) AS line_total
+    FROM    Saleslt.SalesOrderDetail
+    WHERE   SalesOrderID = @orderid ;
